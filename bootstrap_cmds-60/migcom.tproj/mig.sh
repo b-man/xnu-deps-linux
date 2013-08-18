0e83f7539dfe659d1244f8d16b50a4365db7feb7
b-man <mckenzba@gmail.com>
2013-08-18 13:04:49 -0400

    On second thought, just use 'which' to get mktemp path

--- bootstrap_cmds-60/migcom.tproj/mig.sh
+++ bootstrap_cmds-60/migcom.tproj/mig.sh
@@ -88,7 +88,9 @@ cppflags="-D__MACH30__"
 files=
 arch=`/usr/bin/arch`
 
-WORKTMP=`/bin/mktemp -d "${TMPDIR:-/tmp}/mig.XXXXXX"`
+MKTEMP=`which mktemp`
+
+WORKTMP=`${MKTEMP} -d "${TMPDIR:-/tmp}/mig.XXXXXX"`
 if [ $? -ne 0 ]; then
       echo "Failure creating temporary work directory: ${WORKTMP}"
       echo "Exiting..."
